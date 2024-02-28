34ea9d46e8cba34166958adc809d318c2622df94
lucas <lucaspsilvadev@gmail.com>
2024-02-27 21:42:41 -0300

    User entity and resource

---
+++ src/main/java/com/mindi/course/resource/UserResource.java
@@ -0,0 +1,20 @@
+package com.mindi.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mindi.course.entites.User;
+
+@RestController
+@RequestMapping("/user")
+	public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findall(){
+		User u = new User(1L, "bolo", "mogli@gmail.com", "9999999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
